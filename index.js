8ac1e48a8e16d6ef06fca3184e5bf9ce32755034
umeshSinghVerma <usv0308@gmail.com>
2024-01-01 15:14:04 +0530

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require('cors');
 const { searchBook } = require("./searchBook");
 const { getBook } = require("./getBook");
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 
